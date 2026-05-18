de2c7838472529e6d13dccc877859b1dd23655fe
손주환 <sonjuhawn@gmail.com>
2026-05-18 18:15:10 +0900

    강공 시스템 애니메이션 연결

--- Assets/Script/Character/PlayerCombat.cs
+++ Assets/Script/Character/PlayerCombat.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 public class PlayerCombat : MonoBehaviour
 {
     private static readonly int AttackHash = Animator.StringToHash("Attack");
-    private static readonly int HeavyAttackHash = Animator.StringToHash("HeavyAttack");
-    private static readonly int DodgeHash = Animator.StringToHash("Dodge");
+    private static readonly int HeavyAttack0Hash = Animator.StringToHash("HeavyAttack0");
+    private static readonly int HeavyAttack1Hash = Animator.StringToHash("HeavyAttack1");
+    private static readonly int HeavyDashHash = Animator.StringToHash("HeavyDash");
     private static readonly int FinisherHash = Animator.StringToHash("Finisher");
+    private static readonly int DodgeHash = Animator.StringToHash("Dodge");
+
 
 
     private PlayerInputHandler input;
@@ -53,13 +56,20 @@ public class PlayerCombat : MonoBehaviour
 
     private void HandleHeavyAttack()
     {
-        if(leftComboIndex == 3)
+        switch (leftComboIndex)
         {
-            anim.SetTrigger(FinisherHash);
-        }
-        else
-        {
-            anim.SetTrigger(HeavyAttackHash);
+            case 0:
+                anim.SetTrigger(HeavyAttack0Hash);
+                break;
+            case 1:
+                anim.SetTrigger(HeavyDashHash);
+                break;
+            case 2:
+                anim.SetTrigger(HeavyAttack1Hash);
+                break;
+            case 3:
+                anim.SetTrigger(FinisherHash);
+                break;
         }
         // 향후 자리: 스테미나 소모 체크
     }
@@ -69,6 +79,4 @@ public class PlayerCombat : MonoBehaviour
         anim.SetTrigger(DodgeHash);
         // 향후 자리: 쿨타임 체크, i-frame 활성화
     }
-
-    
 }
