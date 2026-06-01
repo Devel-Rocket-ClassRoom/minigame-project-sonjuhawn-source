ae7f9b23fac8e137b0a56d314f70e5ac01268934
손주환 <sonjuhawn@gmail.com>
2026-06-01 15:49:12 +0900

    레벨업 + 웨이브 클리어 동시 발생 시 UI 충돌 버그 수정
    Root Cause
    레벨업(StatPanel)이 상점 오픈보다 먼저 발생하는 타이밍에서 상점 닫기 시 커서/timeScale을 강제 복구해 스탯 패널이 동작 불가 상태가 됨 - 해결

---
+++ Assets/Script/UI/PauseManager.cs
@@ -0,0 +1,23 @@
+// PauseManager 싱글톤
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public static PauseManager Instance { get; private set; }
+    private int pauseCount = 0;
+
+    public int PauseCount => pauseCount;
+
+    private void Awake() => Instance = this;
+
+    public void Pause()
+    {
+        pauseCount++;
+        Time.timeScale = 0f;
+    }
+    public void Resume()
+    {
+        pauseCount = Mathf.Max(0, pauseCount - 1);
+        if (pauseCount == 0) Time.timeScale = 1f;
+    }
+}
